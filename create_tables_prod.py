26920bd389b845a2a6678049598e49830f49a989
root <root@srv1097127.hstgr.cloud>
2025-11-09 15:31:06 +0000

    Update Prod

---
+++ create_tables_prod.py
@@ -0,0 +1,8 @@
+# create_tables_prod.py
+from app import create_app, db
+
+if __name__ == "__main__":
+    app = create_app()
+    with app.app_context():
+        db.create_all()
+        print("All tables created successfully!")
